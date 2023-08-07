58f025616e6a22c6d58bfadda5631203cf1dc310
unknown <tyras63@gmail.com>
2023-08-07 09:57:07 +0300

    added changes

--- src/main/java/io/codelex/flightplanner/domain/Flight.java
+++ src/main/java/io/codelex/flightplanner/domain/Flight.java
@@ -1,32 +1,43 @@
 package io.codelex.flightplanner.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Validated
+@Entity
+@Table(name = "flight")
 public class Flight {
-    @NotBlank
+    // @NotBlank
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "flight_id")
     private long id;
     @Valid
     @NotNull
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "from_airport_id")
     private Airport from;
     @Valid
     @NotNull
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "to_airport_id")
     private Airport to;
     @NotBlank
     private String carrier;
 
     @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @Column(name = "departure_time")
     private LocalDateTime departureTime;
 
     @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @Column(name = "arrival_time")
     private LocalDateTime arrivalTime;
 
     public Flight(long id, Airport from, Airport to, String carrier, LocalDateTime departureTime, LocalDateTime arrivalTime) {
@@ -37,6 +48,10 @@ public class Flight {
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
 
+
+    }
+
+    public Flight() {
 
     }
 
@@ -87,5 +102,30 @@ public class Flight {
 
     public void setArrivalTime(LocalDateTime arrivalTime) {
         this.arrivalTime = arrivalTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return id == flight.id && Objects.equals(from, flight.from) && Objects.equals(to, flight.to) && Objects.equals(carrier, flight.carrier) && Objects.equals(departureTime, flight.departureTime) && Objects.equals(arrivalTime, flight.arrivalTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, carrier, departureTime, arrivalTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", from=" + from +
+                ", to=" + to +
+                ", carrier='" + carrier + '\'' +
+                ", departureTime=" + departureTime +
+                ", arrivalTime=" + arrivalTime +
+                '}';
     }
 }
